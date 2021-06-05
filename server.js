6bd236827156debe90b9691c64ecfc22dda91d61
Cristian Gonzalez <cristian.gonzalez.unq@gmail.com>
2021-06-04 21:17:46 -0300

    refactor

--- server.js
+++ server.js
@@ -1,14 +1,26 @@
-const express = require('express');
-//const favicon = require('express-favicon');
-const path = require('path');
+import express from 'express';
+import path from 'path';
+import './src/Connection.js';
+import User from './src/User.js';
+
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
 const port = process.env.PORT || 8080;
 const app = express();
-//app.use(favicon(__dirname + '/build/favicon.ico'));
-// the __dirname is the current directory from where the script is running
+
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
-app.get('/ping', function (req, res) {
- return res.send('pong');
+
+app.use(express.json());
+/* app.get('/users', function (req, res) {
+  return res.json({ message: 'pong' });
+}); */
+
+app.post('/users', async (req, res) => {
+  console.log(req.boy);
+  const newUser = new User({ userName: req.body.userName, password: req.body.password })
+  console.log(newUser)
+  const userSaved = await newUser.save();
+  return res.json(userSaved);
 });
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
